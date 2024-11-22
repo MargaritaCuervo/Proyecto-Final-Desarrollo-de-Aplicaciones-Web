51afa69f81784d96b0c531f62d8a8b01a892f06a
BuildTools <unconfigured@null.spigotmc.org>
2024-11-21 20:56:07 -0600

    /vocaloid y /fuente_sonido

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def index():
 def nosotros():
     return render_template("nosotros.html")
 
+@app.route('/vocaloid')
+def vocaloid():
+    return render_template("vocaloid.html")
+
+@app.route('/fuente_sonido')
+def fuente_sonido():
+    return render_template("fuente_sonido.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
